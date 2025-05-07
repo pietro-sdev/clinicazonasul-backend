20c2d02711b55bd5cd585d9ad6d746f89eaa66b7
Pietro dos Santos Menezes <pietro.dsantos77@gmail.com>
2025-05-07 13:35:29 -0300

    fix/host liberated

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ app.register(multipart, {
 
 app.register(registerRoutes)
 
-app.listen({ port: 3333 }, (err, address) => {
+app.listen({ port: 3333, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
